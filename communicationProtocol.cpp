918ee430032a0d9b589bb4a1bfb1fa15c1d0fd1e
GhiteaCosmin <ghiteacosmin@gmail.com>
2016-03-19 23:34:34 +0200

    Update communicationProtocol.cpp

--- communicationProtocol.cpp
+++ communicationProtocol.cpp
@@ -7,6 +7,31 @@ int getDutycycle(protocol *buffer) {
   return 0;
 }
 
+int checkForwardCommand(protocol *buffer) {
+  if(buffer->Head == 1 && buffer->interface == 1) {
+    return buffer->data[1];
+  }
+  return 0;
+}
+
+int checkBackwardCommand(protocol *buffer) {
+  if(buffer->Head == 1 && buffer->interface == 2) {
+    return buffer->data[1];
+  }
+  return 0;
+}
+
+int checkMakeLeft(protocol *buffer) {
+  if(buffer->Head == 1 && buffer->interface == 3) {
+    return buffer->data[1];
+  }
+  return 0;
+}
 
 
-
+int checkMakeRight(protocol *buffer) {
+  if(buffer->Head == 1 && buffer->interface == 4) {
+    return buffer->data[1];
+  }
+  return 0;
+}
