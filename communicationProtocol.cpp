19cff48e780cb3bbcfd84c50fc57830b8abe74d5
GhiteaCosmin <ghiteacosmin@gmail.com>
2016-03-19 23:07:14 +0200

    Update communicationProtocol.cpp

--- communicationProtocol.cpp
+++ communicationProtocol.cpp
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 typedef struct {
-  unsigned char Head;
-  unsigned char Interface;
-  unsigned char Data[2];
+  unsigned char head;
+  unsigned char interface;
+  unsigned char data[2];
   unsigned char CRC;
 }protocol;
 
 const char receive;
-const char idInterface;
+const char comandInterface;
+const char information;
+const char datainfomation;
 const char time;
-const char Duty_cycle;
+const char duty_cycle;
+
+int getDutycycle(protocol *buffer) {
+  if(buffer->Head == 1) {
+  return buffer->data[0]
+  }
+  return 0;
+}
+
 
