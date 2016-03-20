3f46a8c493228b50bc3cc0dfc66ba7c65a8dc7d9
Marius Marusanici <marius.marusanici@gmail.com>
2016-03-20 21:56:04 +0200

    Update communicationProtocol.cpp

--- communicationProtocol.cpp
+++ communicationProtocol.cpp
@@ -1,41 +1,40 @@
-#include <communicationProtocol.h>
+#include "communicationProtocol.h"
 
-int getDutycycle(protocol *buffer) {
+int getDutycycle(const tProtocol *buffer) {
   if(buffer->Head == 1) {
-  return buffer->data[0]
+    return buffer->data[0]
   }
   return 0;
 }
 
-int checkForwardCommand(protocol *buffer) {
+int checkForwardCommand(const tProtocol *buffer) {
   if(buffer->Head == 1 && buffer->interface == 1) {
     return buffer->data[1];
   }
   return 0;
 }
 
-int checkBackwardCommand(protocol *buffer) {
+int checkBackwardCommand(const tProtocol *buffer) {
   if(buffer->Head == 1 && buffer->interface == 2) {
     return buffer->data[1];
   }
   return 0;
 }
 
-int checkMakeLeft(protocol *buffer) {
+int checkMakeLeft(const tProtocol *buffer) {
   if(buffer->Head == 1 && buffer->interface == 3) {
     return buffer->data[1];
   }
   return 0;
 }
 
-
-int checkMakeRight(protocol *buffer) {
+int checkMakeRight(const tProtocol *buffer) {
   if(buffer->Head == 1 && buffer->interface == 4) {
     return buffer->data[1];
   }
   return 0;
 }
 
-int getSpeed(protocol *buffer) {
+int getSpeed(const tProtocol *buffer) {
   if(buffer->Send == 2 && )
 }
