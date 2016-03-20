05e8648f196b7ab26d8382359e4657d9f8c32c21
GhiteaCosmin <ghiteacosmin@gmail.com>
2016-03-20 21:42:49 +0200

    Update communicationProtocol.h

--- communicationProtocol.h
+++ communicationProtocol.h
@@ -1,5 +1,19 @@
 #include <stdio.h>
-#define Send 1;
+#define cRead 1;
+#define cSend 2;
+#define cForwoard 1;
+#define cBackword 2;
+#define cMakeleft 3;
+#define cMakeright 4;
+#define cStop 5;
+#define cSpeed 6;
+#define cSign 7;
+#define cSensor1 8;
+#define cSensor2 9;
+#define cSensor3 10;
+#define cSensor4 11;
+#define cSensor5 12;
+#define cSensor6 13;
 
 typedef struct {
   unsigned char head;
